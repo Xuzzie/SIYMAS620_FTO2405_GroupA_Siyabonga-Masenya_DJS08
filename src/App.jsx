bbb05711bf0cc7b596c1287a3443927f3067a032
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-10-02 17:12:18 +0200

    Added Footer component and its css

--- src/App.jsx
+++ src/App.jsx
@@ -16,24 +16,16 @@ function App() {
   return (
     <>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
 
-          {/* 
-          Challenge: 
-          1. Turn the "/host" path into a parent route and nest
-          "/host/income" and "/host/reviews" as children routes.
-          Then try entering "/host/income" in the URL and see what
-          happens.
-          
-          2. See if you can figure out why it did what it did.
-          */}
-          <Route path="/host" element={<HostLayout />}>
-            <Route path="/host/income" element={<Income />} />
-            <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
