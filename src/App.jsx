9db7797102cbffea8cc05b288867a764dd9a3684
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-10-02 16:50:31 +0200

    fixed dashboard issue with not displaying as before pathing issue now creating a hostlayout compoent

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Layout from "./components/Layout.jsx";
 import Dashboard from "./pages/Host/Dashboard.jsx";
 import Income from "./pages/Host/Income.jsx";
 import Reviews from "./pages/Host/Reviews.jsx";
+import HostLayout from "./components/HostLayout.jsx";
 
 import "./server.js";
 
@@ -20,9 +21,20 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+
+          {/* 
+          Challenge: 
+          1. Turn the "/host" path into a parent route and nest
+          "/host/income" and "/host/reviews" as children routes.
+          Then try entering "/host/income" in the URL and see what
+          happens.
+          
+          2. See if you can figure out why it did what it did.
+          */}
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </>
