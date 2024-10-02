278eb1a144ec83af67359e806cc4f6e1f143ed62
Xuzzie <163240835+Xuzzie@users.noreply.github.com>
2024-10-02 15:00:37 +0200

    created Van Details.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans";
+import VanDetail from "./pages/VansDetail.jsx";
 import { Routes, Route, Link } from "react-router-dom";
 
 /**
